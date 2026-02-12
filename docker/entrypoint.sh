a02a1690852704efc39392bf4d9f88becfb65b82
Giovanne Guedes <133803870+gioguedes@users.noreply.github.com>
2026-02-12 09:01:51 -0300

    Modify entrypoint to run Executar_CVDW.sh in background
    Run Executar_CVDW.sh in the background to prevent blocking crond.

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,6 +5,7 @@ envsubst < "/app/src/envs/env.tmpl.ini" > "/app/src/envs/cvdw.env"
 
 php src/cvdw configurar autoupdate --force=true --set-env=cvdw
 
-/app/Executar_CVDW.sh
+# Executa em background para não bloquear o crond
+/app/Executar_CVDW.sh &
 
 exec "$@"
